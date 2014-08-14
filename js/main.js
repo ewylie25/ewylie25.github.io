7050d7305d01f8a16ff5c382d40dc8bec4956872
Liz Wylie <ewylie25@gmail.com>
2014-08-13 23:19:09 -0500

    Interaction working now but I have so much redundant code... I need a JS coder.

--- js/main.js
+++ js/main.js
@@ -1,51 +1,164 @@
-
-
-var enter = function(new_slide){
-	var currentSlide = $('.active-slide');
-    var nextSlide = $(new_slide);
-        
-    currentSlide.fadeOut(600);
-    currentSlide.removeClass("active-slide");
-    nextSlide.fadeIn(600);
-    nextSlide.toggleClass('active-slide');
-};
-
-var exit = function(){
-	var currentSlide = $(".active-slide");
-    var nextSlide = $(".intro");
-        
-    currentSlide.fadeOut(600);
-    currentSlide.removeClass("active-slide");
-    nextSlide.fadeIn(600);
-    nextSlide.addClass("active-slide");
-};
-
-
-
 var main = function(){
-    $('.name').mouseenter(enter('.about_me'));
-    $('.name').mouseleave(exit());
+    var name = $( '.name' );
+    var home = $('.home_link');
+    var resume = $('.res_link');
+    var linked_in =  $('.li_glyph');
+    var github = $('.git_glyph');
+    var google = $('.goog_glyph');
+    var facebook = $('.fb_glyph');
+    var skype = $('.sk_glyph');
+
+    name.mouseenter(function(){
+        var currentSlide = $('.active-slide');
+        var nextSlide = $('.about_me');
+
+        currentSlide.fadeOut(600);
+        currentSlide.removeClass("active-slide");
+        nextSlide.fadeIn(600);
+        nextSlide.addClass("active-slide");
+        });
+    name.mouseleave(function(){
+        var currentSlide = $(".active-slide");
+        var nextSlide = $(".intro");
+
+        currentSlide.fadeOut(600);
+        currentSlide.removeClass("active-slide");
+        nextSlide.fadeIn(600);
+        nextSlide.addClass("active-slide");
+    });
     
-    $('.home_link').mouseenter(enter('.home'));
-    $('.home_link').mouseleave(exit());
+    home.mouseenter(function(){
+        var currentSlide = $('.active-slide');
+        var nextSlide = $('.home');
+
+        currentSlide.fadeOut(600);
+        currentSlide.removeClass("active-slide");
+        nextSlide.fadeIn(600);
+        nextSlide.addClass("active-slide");
+    });
+    home.mouseleave(function(){
+        var currentSlide = $(".active-slide");
+        var nextSlide = $(".intro");
+
+        currentSlide.fadeOut(600);
+        currentSlide.removeClass("active-slide");
+        nextSlide.fadeIn(600);
+        nextSlide.addClass("active-slide");
+    });
     
-    $('.res_link').mouseenter(enter('.resume'));
-    $('.res_link').mouseleave(exit());
+    resume.mouseenter(function(){
+        var currentSlide = $('.active-slide');
+        var nextSlide = $('.resume');
+
+        currentSlide.fadeOut(600);
+        currentSlide.removeClass("active-slide");
+        nextSlide.fadeIn(600);
+        nextSlide.addClass("active-slide");
+    });
+    resume.mouseleave(function(){
+        var currentSlide = $(".active-slide");
+        var nextSlide = $(".intro");
+
+        currentSlide.fadeOut(600);
+        currentSlide.removeClass("active-slide");
+        nextSlide.fadeIn(600);
+        nextSlide.addClass("active-slide");
+    });
+
+    linked_in.mouseenter(function(){
+        var currentSlide = $('.active-slide');
+        var nextSlide = $('.linked_in');
+
+        currentSlide.fadeOut(600);
+        currentSlide.removeClass("active-slide");
+        nextSlide.fadeIn(600);
+        nextSlide.addClass("active-slide");
+    });
+    linked_in.mouseleave(function(){
+        var currentSlide = $(".active-slide");
+        var nextSlide = $(".intro");
+
+        currentSlide.fadeOut(600);
+        currentSlide.removeClass("active-slide");
+        nextSlide.fadeIn(600);
+        nextSlide.addClass("active-slide");
+    });
     
-    $('.li_glyph').mouseenter(enter('.linked_in'));
-    $('.li_glyph').mouseleave(exit());
+    github.mouseenter(function(){
+        var currentSlide = $('.active-slide');
+        var nextSlide = $('.github');
+
+        currentSlide.fadeOut(600);
+        currentSlide.removeClass("active-slide");
+        nextSlide.fadeIn(600);
+        nextSlide.addClass("active-slide");
+    });
+    github.mouseleave(function(){
+        var currentSlide = $(".active-slide");
+        var nextSlide = $(".intro");
+
+        currentSlide.fadeOut(600);
+        currentSlide.removeClass("active-slide");
+        nextSlide.fadeIn(600);
+        nextSlide.addClass("active-slide");
+    });
     
-    $('.git_glyph').mouseenter(enter('.github'));
-    $('.git_glyph').mouseleave(exit());
+    google.mouseenter(function(){
+        var currentSlide = $('.active-slide');
+        var nextSlide = $('.google_plus');
+
+        currentSlide.fadeOut(600);
+        currentSlide.removeClass("active-slide");
+        nextSlide.fadeIn(600);
+        nextSlide.addClass("active-slide");
+    });
+    google.mouseleave(function(){
+        var currentSlide = $(".active-slide");
+        var nextSlide = $(".intro");
+
+        currentSlide.fadeOut(600);
+        currentSlide.removeClass("active-slide");
+        nextSlide.fadeIn(600);
+        nextSlide.addClass("active-slide");
+    });
     
-    $('.goog_glyph').mouseenter(enter('.google_plus'));
-    $('.goog_glyph').mouseleave(exit());
+    facebook.mouseenter(function(){
+        var currentSlide = $('.active-slide');
+        var nextSlide = $('.facebook');
+
+        currentSlide.fadeOut(600);
+        currentSlide.removeClass("active-slide");
+        nextSlide.fadeIn(600);
+        nextSlide.addClass("active-slide");
+    });
+    facebook.mouseleave(function(){
+        var currentSlide = $(".active-slide");
+        var nextSlide = $(".intro");
+
+        currentSlide.fadeOut(600);
+        currentSlide.removeClass("active-slide");
+        nextSlide.fadeIn(600);
+        nextSlide.addClass("active-slide");
+    });
     
-    $('.fb_glyph').mouseenter(enter('.facebook'));
-    $('.fb_glyph').mouseleave(exit());
-    
-    $('.sk_glyph').mouseenter(enter('.skype'));
-    $('.sk_glyph').mouseleave(exit());
+    skype.mouseenter(function(){
+        var currentSlide = $('.active-slide');
+        var nextSlide = $('.skype');
+
+        currentSlide.fadeOut(600);
+        currentSlide.removeClass("active-slide");
+        nextSlide.fadeIn(600);
+        nextSlide.addClass("active-slide");
+    });
+    skype.mouseleave(function(){
+        var currentSlide = $(".active-slide");
+        var nextSlide = $(".intro");
+
+        currentSlide.fadeOut(600);
+        currentSlide.removeClass("active-slide");
+        nextSlide.fadeIn(600);
+        nextSlide.addClass("active-slide");
+    });
 	 
 };
 
